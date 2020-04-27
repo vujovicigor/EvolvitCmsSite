d8f96ae1d03e39f411d2823f2737ddb24970224d
igor <igor@IgorBuildconMBP.local>
2020-04-27 09:53:17 +0200

    mobile fix

--- js/custom.js
+++ js/custom.js
@@ -33,15 +33,17 @@ function loadGoogleMap(){
 }
 
 $(function(){
-  loadGoogleMap();
+  //loadGoogleMap();
 });
 
 // NIVO LIGHTBOX
+/*
 $('#portfolio a').nivoLightbox({
         effect: 'fadeScale',
     });
-
+*/
 // HIDE MOBILE MENU AFTER CLIKING ON A LINK
+
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
