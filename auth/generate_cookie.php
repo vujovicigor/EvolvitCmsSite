281cebb517b4506b672e9b096bac837d097e4a90
igor <igor@IgorBuildconMBP.local>
2020-05-17 14:26:50 +0200

    playground test cookie serialize 30 days

--- auth/generate_cookie.php
+++ auth/generate_cookie.php
@@ -12,7 +12,8 @@ $message = json_encode( $from_db );
 $ciphertext = safeEncrypt($message, $key);
 $plaintext = safeDecrypt($ciphertext, $key);
 $duration = 60*60*24*30; // 30 days
-setcookie('playground', $plaintext, time() + $duration, '/', '.evolvitcms.com');
+
+setcookie('playground', $ciphertext, time() + $duration, '/', '.evolvitcms.com');
 
 var_dump($ciphertext);
 var_dump($plaintext);
