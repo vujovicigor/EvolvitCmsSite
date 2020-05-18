06e475fd474d387edf0a52b37d42bd8eb530c873
igor <igor@IgorBuildconMBP.local>
2020-05-18 13:08:32 +0200

    gauth db save

--- auth/googletokensignin.php
+++ auth/googletokensignin.php
@@ -15,16 +15,36 @@ curl_close($curl);
 
 $userdata = json_decode($resp, TRUE);
 session_start();
-echo $resp;
+//echo $resp;
+
+////////////////
+//session_start();
+//print_r( $_POST );
+//print_r( $_SESSION );
+
+
 //header('Access-Control-Allow-Origin: *');
-//header('Content-Type: application/json');
-/*
+header('Content-Type: application/json');
+
 $_SESSION['_session_google_user_sub'] = $userdata['sub'];
 $_SESSION['_session_google_user_email'] = $userdata['email'];
 $_SESSION['_session_google_user_picture'] = $userdata['picture'];
 $_SESSION['_session_google_user_given_name'] = $userdata['given_name'];
 $_SESSION['_session_google_user_family_name'] = $userdata['family_name'];
 $_SESSION['_session_google_user_locale'] = $userdata['locale'];
-*/
-//echo $resp;
 
+echo $resp;
+
+include("engine.php");
+//$userdata = json_decode($resp, TRUE);
+//header('Access-Control-Allow-Origin: *');
+//header('Content-Type: application/json');
+
+//$_SESSION['_session_post_count'] = 1;
+$f = fetch('
+insert into `PlaygroundUsers` 
+( `google_user_email`, `google_user_family_name`, `google_user_given_name`, `google_user_locale`, `google_user_picture`, `google_user_sub`) 
+values 
+( :_session_google_user_email, :_session_google_user_family_name, :_session_google_user_given_name, :_session_google_user_locale, :_session_google_user_picture, :_session_google_user_sub)
+', $_POST);
+print_r( $f );
